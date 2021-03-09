0781f35112227990b8b19b968b2e1ca1ef9377cf
Josh Leath <lawnfurniture@icloud.com>
2021-03-09 13:32:34 -0500

    completed simple expense tracking app

--- schema.sql
+++ schema.sql
@@ -2,5 +2,5 @@ CREATE TABLE expenses (
     id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     amount numeric(6, 2) NOT NULL CHECK (amount > 0.00),
     memo text NOT NULL,
-    created_on date NOT NULL
+    created_on date NOT NULL DEFAULT NOW()
 );
